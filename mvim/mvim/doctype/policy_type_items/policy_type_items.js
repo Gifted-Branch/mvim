faf45bffde0cf9f4d49ba420cd5c0b278aa61ec6
Douglas <nkubitudouglas@gmail.com>
2023-11-04 22:36:49 +0300

    feat: Initialize App

---
+++ mvim/mvim/doctype/policy_type_items/policy_type_items.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Douglas Nkubitu and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Policy Type Items', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ mvim/mvim/doctype/policy_type_items/policy_type_items.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Douglas Nkubitu and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class PolicyTypeItems(Document):
+	pass
