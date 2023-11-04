49925ad3ed2e39fdae2191fea0867cde8669f28f
Douglas <nkubitudouglas@gmail.com>
2023-11-04 23:22:36 +0300

    feat: qr code in print format

--- mvim/mvim/doctype/motor_vehicle_insurance_certificate/motor_vehicle_insurance_certificate.py
+++ mvim/mvim/doctype/motor_vehicle_insurance_certificate/motor_vehicle_insurance_certificate.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2023, Douglas Nkubitu and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from mvim.qr_code import get_qr_code
 
 class MotorVehicleInsuranceCertificate(Document):
-	pass
+	def validate(self):
+		self.qr_code = get_qr_code(
+			self.vehicle_registration_no,
+			# self.policy_type,
+			# self.chasis_no
+		)
